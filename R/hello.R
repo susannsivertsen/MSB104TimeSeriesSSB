a7737b0576f973aadd74781209ab5a4e08401cfc
susannsivertsen <89196009+susannsivertsen@users.noreply.github.com>
2021-10-13 13:42:14 +0200

    .

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,11 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+#' This is a hello function with a name argument
+#' @export
+hello <- function(name=NULL) {
+  tekst <- paste("Hello world! My name is", name)
+  print(tekst)
 }
+
+.libPaths
---
+++ data-raw/DATASET.R
@@ -0,0 +1,8 @@
+## code to prepare `DATASET` dataset goes here
+
+
+
+## code to prepare `DATASET` dataset goes here
+hw_iris <- iris
+usethis::use_data(hw_iris, overwrite = TRUE)
+
