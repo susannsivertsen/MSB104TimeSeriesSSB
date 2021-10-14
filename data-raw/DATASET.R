b8ff89c29ac8b3d96e5264a3a8458c3671098b10
susannsivertsen <89196009+susannsivertsen@users.noreply.github.com>
2021-10-14 13:47:21 +0200

    .

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,3 +6,7 @@
 hw_iris <- iris
 usethis::use_data(hw_iris, overwrite = TRUE)
 
+### Run once to configure your package to use pkgdown
+usethis::use_pkgdown()
+## Run to build the website
+pkgdown::build_site()
